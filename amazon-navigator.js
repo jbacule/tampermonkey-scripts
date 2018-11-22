9d0a037f9c0f45a41579dc1c6e163bceb3fd2c6d
Josh Bacule <joshue@outdoorequipped.com>
2018-11-23 04:31:12 +0800

    Update amazon-navigator.js

--- amazon-navigator.js
+++ amazon-navigator.js
@@ -1,21 +1,11 @@
-// ==UserScript==
-// @name         amazon-navigator
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.amazon.com/*
-// @grant        none
-// ==/UserScript==
-
 (function() {
     'use strict';
     document.body.onkeyup = function(e){
         if(e.ctrlKey && e.keyCode == 37){ // ctrl key + enter
             document.querySelector('#olpProductImage > a').click();
         }else if(e.ctrlKey && e.keyCode == 39){ // ctrl key + '+'
-            let asin = window.location.href.split('/')[5];
-            window.location.href = 'https://www.amazon.com/dp/offer-listing/'+asin;
+            let asin = window.location.href.substring(window.location.href.indexOf('/B0')+1, window.location.href.indexOf('/B0')+11);
+            window.location.href = 'https://www.amazon.com/dp/offer-listing/' + asin;
         }
     }
 })();
