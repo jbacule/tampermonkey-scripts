baace80331778f3cd277373f984bc2d1730e2939
Josh Bacule <joshue@outdoorequipped.com>
2018-11-26 06:31:09 +0800

    Update purge-and-replace-hider.js

--- purge-and-replace-hider.js
+++ purge-and-replace-hider.js
@@ -4,14 +4,14 @@
 // @version      0.1
 // @description  hide purge and replace area in seller central upload
 // @author       Josh
-// @match        https://sellercentral.amazon.com/listing/*
+// @match        https://sellercentral.amazon.com/*
 // @grant        GM_notification
 // ==/UserScript==
 
 (function() {
     'use strict';
     $(document).ready(function(){
-         if(window.location.href.indexOf('/listing/upload')>-1){
+         if(window.location.href.indexOf('/listing/upload')>-1 || window.location.href.indexOf('/listing')>-1){
            $('#vlw-container > div.a-row > div.a-column.a-span9 > div.a-row.a-expander-container.a-expander-section-container.a-section-expander-container').hide();
            GM_notification( { title: "Reminder", text: "Purge and replace hidden!", timeout: 3000, image: "https://png.icons8.com/color/64/000000/ok.png" } );
          }
