d411b63f0d6e02acfdbd5afbdcf5d99789650888
Josh Bacule <joshue@outdoorequipped.com>
2019-05-06 18:06:41 +0800

    Update purge-and-replace-hider.js

--- purge-and-replace-hider.js
+++ purge-and-replace-hider.js
@@ -7,13 +7,14 @@
 // @match        https://sellercentral.amazon.com/*
 // @grant        GM_notification
 // @run-at       document-start
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     $(document).ready(function(){
-        var uploadHeader = $('#tabs-addproducts-upload-heading').attr('class');
-        if( uploadHeader == 'a-tab-heading a-active'){
+        var uploadHeader = $('span.a-color-error').text();
+        if( uploadHeader == 'Purge and Replace Your Inventory'){
            $('#vlw-container > div.a-row > div.a-column.a-span9 > div.a-row.a-expander-container.a-expander-section-container.a-section-expander-container').hide();
            GM_notification( { title: "Reminder", text: "Purge and replace was hide!", timeout: 3000, image: "https://img.icons8.com/color/64/000000/appointment-reminders.png" } );
          }
